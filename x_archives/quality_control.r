eb5138112798e29083c5710ef6e8e8f2df83d7cf
TanaraRT <tanara.truong@gmail.com>
2025-09-09 11:35:11 +0200

    Deleted tmp files and archives

--- x_archives/quality_control.r
+++
@@ -1,7 +0,0 @@
-## Quality control: eventRemarks columns
-
-quality_control <- function(dt, col) {
-  year_pattern <- "^\\d{4}$"
-  dt[grepl(year_pattern, get(col)), eventRemarks := "high confidence"]
-  return(dt)
-}
